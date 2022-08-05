e141a42c1aad1dc8d9ae9ae8f3e4f818dfdc6411
Mohamed Fadul <72509281+mofadul1@users.noreply.github.com>
2022-08-05 21:34:13 +0000

    finalized

--- script.js
+++ script.js
@@ -10,39 +10,25 @@ function jump() {
   }, 300);
 }
 
-
 var checkDead = setInterval(function() {
-  var charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); // return top position of character.
-  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left")); // return top position of character.
+  let charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); // return top position of character.
+  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left")); // return top position of character.
+
+
   if (blockLeft < 20 && blockLeft > -20 && charTop >= 130) {
     block.style.animation = "none";
-    block.style.display = "none";
+    // block.style.display = "none";
     alert("L..... Do better. Score: " + Math.floor(counter/100));
     counter = 0;
     block.style.animation = "block 1s infinite linear";
+    location.reload();
   } else {
     counter++;
     document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
   }
 }, 10);
-
-
-
-      
-        
-        
         
     
-        
-    
-
-
-
-
-
-
-
-
-
+  
 
 
